220d9e2e645c1c64a0c1eeb1b89a9856c24d4840
kuroki <shuntaro.kuroki@livesense.co.jp>
2017-03-03 10:41:49 +0900

    ReactのHMR用のloader追加

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'react-hmre']
         }
       },
       {
