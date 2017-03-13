8686ca09c9344a2ccc8b28d01e752ce2eda567e1
kuroki <shuntaro.kuroki@livesense.co.jp>
2017-03-13 14:10:15 +0900

    コード整理

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import styles from './styles'
 
 const Display = ({children}) => (
-  <div className={`${styles}`}>
+  <div className={styles}>
     {children}
   </div>
 )
--- src/components/NumberPanel/index.js
+++ src/components/NumberPanel/index.js
@@ -2,30 +2,32 @@ import React, { Component, PropTypes } from 'react'
 import { buttonStyle } from './styles'
 import { css } from 'glamor'
 
-const NumberButton = ({children, onPush, optionStyle}) => (
-  <button className={css(buttonStyle, optionStyle)} onClick={onPush}>{children}</button>
+const NumberButton = ({children, handleClick, handleKeyPress, optionStyle}) => (
+  <button className={css(buttonStyle, optionStyle)} onClick={handleClick}>{children}</button>
 )
 
 const NumberPanel = ({onAppendNumber}) => {
   return (
     <div>
       <div>
-        <NumberButton onPush={() => { onAppendNumber(7) }}>7</NumberButton>
-        <NumberButton onPush={() => { onAppendNumber(8) }}>8</NumberButton>
-        <NumberButton onPush={() => { onAppendNumber(9) }}>9</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(7) }}>7</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(8) }}>8</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(9) }}>9</NumberButton>
       </div>
       <div>
-        <NumberButton onPush={() => { onAppendNumber(4) }}>4</NumberButton>
-        <NumberButton onPush={() => { onAppendNumber(5) }}>5</NumberButton>
-        <NumberButton onPush={() => { onAppendNumber(6) }}>6</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(4) }}>4</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(5) }}>5</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(6) }}>6</NumberButton>
       </div>
       <div>
-        <NumberButton onPush={() => { onAppendNumber(1) }}>1</NumberButton>
-        <NumberButton onPush={() => { onAppendNumber(2) }}>2</NumberButton>
-        <NumberButton onPush={() => { onAppendNumber(3) }}>3</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(1) }}>1</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(2) }}>2</NumberButton>
+        <NumberButton handleClick={() => { onAppendNumber(3) }}>3</NumberButton>
       </div>
       <div>
-        <NumberButton onPush={() => { onAppendNumber(0) }} optionStyle={css({width: '150px'})}>0</NumberButton>
+        <NumberButton
+          handleClick={() => { onAppendNumber(0) }}
+          optionStyle={css({width: '150px'})}>0</NumberButton>
       </div>
     </div>
   )
